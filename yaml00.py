cf13222244a64fcfe56adedfa6bc708a64f4920c
hector <hvaldecantos@gmail.com>
2019-07-03 15:45:08 -0400

    adds nested key-value in yaml

--- yaml00.py
+++ yaml00.py
@@ -5,13 +5,19 @@ import io
 data = {'participant': 1,
         'warmup': True,
         'pretest': ['t', 'f', 'dk', 'dk', 't', 't', 't', 'f', 'dk', 'dk', 't'],
-        'system example': ['library', 'spell'],
-        'task': [2, 4, 3]}
+        'system example': [
+        	{'library': {'t1': ['t', 'f', 'f'], 
+        				 't3': ['t', 'f', 'f'],
+        				 't4': ['t', 'f', 'f'],
+        				 't2': ['t', 'f', 'f']},
+        	 'spell' : {'t2' : ['t', 't', 'dk']}}
+        	 ],
+        }
 
 
 # Write YAML file
 with io.open('data.yaml', 'w', encoding='utf8') as outfile:
-    yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True)
+    yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True, sort_keys=False)
 
 # Read YAML file
 with open("data.yaml", 'r') as stream:
